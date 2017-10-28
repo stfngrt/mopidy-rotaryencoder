b54d7b8079bf414b1fe79061b33e41c6350707d6
Matthias Kreuzriegler <matthias@kreuzriegler.at>
2017-10-28 21:03:48 +0200

    use integer instead of string

--- mopidy_rotaryencoder/__init__.py
+++ mopidy_rotaryencoder/__init__.py
@@ -21,9 +21,9 @@ class Extension(ext.Extension):
 
     def get_config_schema(self):
         schema = super(Extension, self).get_config_schema()
-        schema['datapin'] = config.String()
-        schema['clkpin'] = config.String()
-        schema['swpin'] = config.String()
+        schema['datapin'] = config.Integer()
+        schema['clkpin'] = config.Integer()
+        schema['swpin'] = config.Integer()
         return schema
 
     def setup(self, registry):
